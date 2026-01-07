20d6eee76d4102e06302431d67e92b8e726561d9
abbasqamar <abbasqamar@msn.com>
2026-01-06 21:52:03 -0800

    ok done

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('asn_health_care_fe_v1');
+}
