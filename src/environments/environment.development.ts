4d81477a789adfc4ed35045394988a691b2f3b79
abbasqamar <abbasqamar@msn.com>
2026-03-25 21:18:33 +0500

    ok

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,6 @@ export const environment = {
 
   production: false, // often true for staging
   //BaseServiceUrl : 'https://asnhealthcarebev1-staging.up.railway.app', // your NestJS login API
-  BaseServiceUrl : 'http://asnhealthcare.org', // your NestJS login API
+  BaseServiceUrl : 'https://asnhealthcare.org', // your NestJS login API
 
 };
